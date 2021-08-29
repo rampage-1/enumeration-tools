b1fdfe45d0d43ba91fe4244c113de2e64382e0bb
rampage-1 <86386432+rampage-1@users.noreply.github.com>
2021-08-29 15:22:34 -0400

    Update brute-subdomains.sh

--- brute-subdomains.sh
+++ brute-subdomains.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-# for Bug Bounties With Bash - VirSecCon2020 Talk, By TomNomNom
+# for Bug Bounties With Bash - VirSecCon2020 Talk
 # https://www.youtube.com/watch?v=s9w0KutMorE
 # not an original work
 #######################
 # bash-based subdomain brute-force enumeration script
-# usage: ./brute-subdomains [domain] < [wordlist]
 
 domain=$1
 
@@ -12,7 +11,5 @@ while read sub; do
     subdomain=$sub.$domain
     if host "$subdomain" &> /dev/null; then
         echo "$subdomain resolves";
-    else
-        echo "$subdomain does not resolve"
     fi
 done
