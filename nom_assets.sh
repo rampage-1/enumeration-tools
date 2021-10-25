098ea8fa4bf3e933098c2a7c32522062768ff83d
rampage-1 <86386432+rampage-1@users.noreply.github.com>
2021-10-25 17:36:07 -0400

    Update and rename nom_domain_assets.sh to nom_assets.sh

---
+++ nom_assets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# for https://academy.tcm-sec.com/p/practical-ethical-hacking-the-complete-course
+# not an original work
+#
+# harvest potential subdomains with tomnomnom's Assetfinder
+# https://github.com/tomnomnom/assetfinder
+# Usage: ./nom_assets.sh <domain>
+
+domain=$1
+
+# directory creation
+now=$(date +%s)
+dir=$domain/recon/$now
+
+#set output directory
+mkdir -p $dir
+
+echo "___________________________________________________________"
+echo ""
+echo "[+] Harvesting subdomains with tomnomnom's assetfinder..."
+
+assetfinder $domain >> $dir/temp-output
+cat $dir/temp-output | sort -u >> $dir/assetfinder_found.txt && rm $dir/temp-output
+
+echo "[+] Probing for live domains ... "
+
+cat $dir/assetfinder_found.txt | httprobe -prefer-https | sort -u >> $dir/live-endpoints.txt
+# the lecture script checked https but I've decided to collect both
+
+echo "[+] Done! "
+
